9ffd384b5d11bda461e97fc1594733d7ea994c0d
adindaivankamp <145529737+adindaivankamp@users.noreply.github.com>
2023-10-04 12:44:07 +0700

    Add files via upload

---
+++ ContohVariabel01.java
@@ -0,0 +1,13 @@
+public class ContohVariabel01 {
+    public static void main (String[] args){
+        String salahSatuHobySayaAdalah = "Berenang";
+        boolean isPandai = true;
+        char jenisKelamin = 'P';
+        byte _umurSayaSekarang = 18;
+        double $ipk = 3.24, tinggi = 1.56;
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini : " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
